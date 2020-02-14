4d8a7012adf3730c595cd77c9d62e0ab6e2d76bb
nicholasfarone <nicholas.farone@snhu.edu>
2020-02-14 13:12:43 -0500

    Added die class

---
+++ Die.cpp
@@ -0,0 +1,4 @@
+//
+// Created by cardb on 2/14/2020.
+//
+
---
+++ Die.h
@@ -0,0 +1,13 @@
+//
+// Created by cardb on 2/14/2020.
+//
+
+#ifndef CRAPS_DIE_H
+#define CRAPS_DIE_H
+class Die{
+private:
+    int numSides;
+    int increment;
+    int value;
+};
+#endif //CRAPS_DIE_H
